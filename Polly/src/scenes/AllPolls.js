542e4f9f0e32f8d4e76f958b2b235b5f71e5508f
Ram Goli <ramsgoli@gmail.com>
2019-11-12 20:42:17 -0800

    got bottom navigation bar to work

---
+++ Polly/src/scenes/AllPolls.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class AllPolls extends Component {
+  render() {
+    return (
+      <View>
+        <Text>All Polls</Text>
+      </View>
+    )
+  }
+}
+
+export default AllPolls
