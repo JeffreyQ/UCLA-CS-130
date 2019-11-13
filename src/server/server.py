08a19358cdf6656c179cc1a345b0c6c9a413af45
Lawrence Chen <lawrencechen98@gmail.com>
2019-11-12 19:15:52 -0800

    Exploring use of flask_restplus for documentation

--- src/server/server.py
+++ src/server/server.py
@@ -1,12 +1,19 @@
-from flask import Flask, request
+from flask import Flask, Blueprint, request
 from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
+from flask_restplus import Api, Resource
 from gevent.pywsgi import WSGIServer
-from models import User, Poll, db
+import models
 import random
 import logging
 
+db = models.db
 app = Flask(__name__)
+
+api = Api(app, version='1.0', title='Polly API',
+    description='Polly API',
+)
+
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///polly'
 db.init_app(app)
@@ -18,7 +25,7 @@ def test():
 @app.route('/users')
 def get_all_users():
     print("TEST")
-    asdf = User.query.all()
+    asdf = models.User.query.all()
     for user in asdf:
         print(user)
     return "Working!"
@@ -26,7 +33,7 @@ def get_all_users():
 @app.route('/add')
 def populate():
     try:
-        test= User(email="test")
+        test= models.User(email="test")
         db.session.add(test)
         db.session.commit()
         return "success"
@@ -34,60 +41,58 @@ def populate():
         print(e)
     return "failure"
 
-@app.route('/create_poll', methods=['POST'])
-def create_poll():
-    try:
-        req_data = request.get_json()
-        owner_id = req_data['owner_id']
-        prompt = req_data['prompt']
-        form_type = req_data['form_type']
-        resp_struct = req_data['resp_struct']
+@api.route('/poll')
+class Poll(Resource):
+    def post(self):
+        try:
+            req_data = request.get_json()
+            owner_id = req_data['owner_id']
+            prompt = req_data['prompt']
+            form_type = req_data['form_type']
+            resp_struct = req_data['resp_struct']
 
-        poll = Poll(owner_id=owner_id, prompt=prompt, form_type=form_type, resp_struct=resp_struct)
-        db.session.add(poll)
-        db.session.commit()
-        return {"poll_id": poll.id}
-    except Exception as e:
-        print(e)
-    return "failure"
-
-
-@app.route('/get_poll', methods=['GET'])
-def get_poll():
-    try:
-        req_data = request.get_json()
-
-        resp = {}
-        polls = []
-        
-        if req_data and "poll_id" in req_data:
-            polls = Poll.query.filter(Poll.id == req_data["poll_id"]).all()
-        else:
-            polls = Poll.query.all()
-        
-        resp["polls"] = [poll.as_dict() for poll in polls]
-        return resp
-    except Exception as e:
-        print(e)
-    return "failure"
-
-@app.route('/delete_poll', methods=['DELETE'])
-def delete_poll():
-    try:
-        req_data = request.get_json()
-        poll = Poll.query.filter(Poll.id == req_data["poll_id"]).first()
-
-        if poll is None:
-            return "Poll {} not found, no deletion necessary.".format(req_data["poll_id"])
-        elif poll.owner_id == None or poll.owner_id == req_data["owner_id"]:
-            db.session.delete(poll)
+            poll = Poll(owner_id=owner_id, prompt=prompt, form_type=form_type, resp_struct=resp_struct)
+            db.session.add(poll)
             db.session.commit()
-            return "Poll {} successfully deleted.".format(req_data["poll_id"])
+            return {"poll_id": poll.id}
+        except Exception as e:
+            print(e)
+        return "failure"
 
-        return "Owner {} does not own poll {}, cannot be deleted.".format(req_data["owner_id"], req_data["poll_id"])
-    except Exception as e:
-        print(e)
-    return "failure"
+    def get(self):
+        try:
+            req_data = request.get_json()
+
+            resp = {}
+            polls = []
+            
+            if req_data and "poll_id" in req_data:
+                polls = Poll.query.filter(Poll.id == req_data["poll_id"]).all()
+            else:
+                polls = Poll.query.all()
+            
+            resp["polls"] = [poll.as_dict() for poll in polls]
+            return resp
+        except Exception as e:
+            print(e)
+        return "failure"
+
+    def delete(self):
+        try:
+            req_data = request.get_json()
+            poll = Poll.query.filter(Poll.id == req_data["poll_id"]).first()
+
+            if poll is None:
+                return "Poll {} not found, no deletion necessary.".format(req_data["poll_id"])
+            elif poll.owner_id == None or poll.owner_id == req_data["owner_id"]:
+                db.session.delete(poll)
+                db.session.commit()
+                return "Poll {} successfully deleted.".format(req_data["poll_id"])
+
+            return "Owner {} does not own poll {}, cannot be deleted.".format(req_data["owner_id"], req_data["poll_id"])
+        except Exception as e:
+            print(e)
+        return "failure"
 
 
 if __name__ == '__main__':
