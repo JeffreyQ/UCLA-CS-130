26a972cd7e3b6df29d04e95967641faf5512447f
Jeffrey Qiu <jeffreyyqiu@gmail.com>
2019-11-19 06:39:07 -0800

    spaced out buttons

--- Polly/src/scenes/CreatePoll/index.js
+++ Polly/src/scenes/CreatePoll/index.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import { Text, View, TextInput } from 'react-native'
+import { Text, View, TextInput, StyleSheet } from 'react-native'
 import { ThemeProvider, Button, Input } from 'react-native-elements'
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-
-const theme = {
-  Button: {
-    containerStyle: {
-      margin: 10,
-      width: '75%',
-      height: 50,
-    }
-  }
-}
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
+import { heading2Text } from '../../textMixins'
 
 class SelectAllPollCreationScreen extends React.Component {
     render() {
@@ -121,27 +112,34 @@ class CreatePollScreen extends React.Component {
       const {navigate} = this.props.navigation;
       return(
         <ThemeProvider theme={theme}>
-          <View style={{flex: 1, alignItems: 'center'}}> 
-            <Text> Select Poll Type </Text>
+          <View style={styles.view}> 
             <Button
-                title="Select All"
-                type="solid"
-                onPress={() => navigate('SelectAllPoll', {name: 'Select All'})}
+              style={styles.container}
+              title="True False"
+              type="clear"
+              onPress={() => navigate('MultipleChoicePoll', {name: 'Select All'})}
             />
             <Button
-                title="Multiple Choice"
-                type="solid"
-                onPress={() => navigate('MultipleChoicePoll', {name: 'Select All'})}
+              style={styles.container}
+              title="Short Answer"
+              type="clear"
+              onPress={() => navigate('ShortAnswerPoll', {name: 'Select All'})}
+           />
+            <Button
+              style={styles.container}
+              title="Number Scale"
+              type="clear"
+              onPress={() => navigate('NumberScalePoll', {name: 'Select All'})}
             />
             <Button
-                title="Short Answer"
-                type="solid"
-                onPress={() => navigate('ShortAnswerPoll', {name: 'Select All'})}
-            />
-            <Button
-                title="Number Scale"
-                type="solid"
-                onPress={() => navigate('NumberScalePoll', {name: 'Select All'})}
+              style={styles.container}
+              title="Select All"
+              type="clear"
+              titleProps={{
+                color: 'black',
+                fontSize: 36
+              }}
+              onPress={() => navigate('SelectAllPoll', {name: 'Select All'})}
             />
           </View>
         </ThemeProvider>
@@ -158,4 +156,66 @@ const MainNavigator = createStackNavigator({
 });
 
 
-  export default createAppContainer(MainNavigator)
+export default createAppContainer(MainNavigator)
+
+const theme = {
+  Button: {
+    containerStyle: {
+      margin: 10,
+      width: '85%',
+      // height: 50,
+    }
+  }
+}
+function elevationShadowStyle(elevation) {
+  return {
+    elevation,
+    shadowColor: '#0047FF',
+    shadowOffset: { width: 0, height: 0.5 * elevation },
+    shadowOpacity: 0.3,
+    shadowRadius: 0.8 * elevation,
+    shadowOpacity: 0.12
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    // marginTop: 28,
+    backgroundColor: 'white',
+    height: 118,
+    padding: 7,
+    // borderTopLeftRadius: 50,
+    // borderBottomLeftRadius: 50,
+    ...elevationShadowStyle(5)
+  },
+  heading2Text: {
+    ...heading2Text
+  },
+  view: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly'
+  },
+  boxWithShadow: {
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowOpacity: 0.43,
+    shadowRadius: 9.51,
+    elevation: 10
+  },
+  text: {
+    // fontFamily: "Baskerville",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontSize: 36
+  },
+  button: {
+    height: 118,
+    width: 303,
+    left: 36,
+    top: 77
+  }
+});
