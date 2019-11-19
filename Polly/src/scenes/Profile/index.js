bbaead561c6b95ac22963b378d0265545bd9b9a4
Ram Goli <ramsgoli@gmail.com>
2019-11-18 19:24:02 -0800

    heading plus body skeleton

--- Polly/src/scenes/Profile/index.js
+++ Polly/src/scenes/Profile/index.js
@@ -3,38 +3,19 @@ import { Image, StyleSheet, Text, SafeAreaView, View } from 'react-native'
 import { ScrollView } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 
-import { myPoll } from '../../store'
 import Heading from './heading'
+import ProfileBody from './body'
 import Card from './card'
 
 class ProfileScreen extends React.Component{
-    render() {
-        return (
-          <SafeAreaView style={styles.container}>
-            <View style={styles.viewContainer}>
-              <Image source={require('./profile.jpg')} style={styles.profilePic}/>
-              <Text style={styles.username}>Username</Text>
-            </View>
-            <Text style={styles.heading}>My Polls</Text>
-            <View style={styles.container}>
-              <ScrollView
-                contentContainerStyle={{paddingBottom:20}}
-                style={{
-                  height:'100%',
-                  alignSelf:'stretch',
-                  paddingRight: 20,
-                  paddingLeft: 20
-                }}>
-                  <Card poll={myPoll} />
-                  <Card poll={myPoll} />
-                  <Card poll={myPoll} />
-                  <Card poll={myPoll} />
-              
-              </ScrollView>
-            </View>
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Heading />
+        <ProfileBody />
       </SafeAreaView>
-        )
-      }
+    )
+  }
 }
 
 const styles = StyleSheet.create({
