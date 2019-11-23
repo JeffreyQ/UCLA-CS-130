7934394536c7fc19fe25c7cdac7027aec3736dab
Jeffrey Qiu <jeffreyyqiu@gmail.com>
2019-11-23 08:12:39 -0800

    resolve merge conflicts

--- Polly/src/scenes/Profile/index.js
+++ Polly/src/scenes/Profile/index.js
@@ -6,36 +6,15 @@ import { heading1Text, bodyText, grayBody } from '../../textMixins'
 import Card from './card'
 
 import Heading from './heading'
-<<<<<<< HEAD
-
-class ProfileScreen extends React.Component{
-  onCardPress = pollId => {
-    return this.props.navigation.navigate('PollResponse', {
-      pollId
-    })
-  }
-
-=======
 import ProfileBody from './body'
 import Card from './card'
 
 class ProfileScreen extends React.Component{
->>>>>>> da5958119e14ba353eb160ffbfe91eff669f6ea8
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <Heading />
-<<<<<<< HEAD
-        <View style={styles.bodyContainer}>
-          <View style={styles.heading}>
-            <Text style={{...heading1Text}}>My Polls</Text>
-            <Text style={{...bodyText, ...grayBody}}>Filter</Text>
-          </View>
-          <Card onPress={() => this.onCardPress(1)}/>
-        </View>
-=======
         <ProfileBody />
->>>>>>> da5958119e14ba353eb160ffbfe91eff669f6ea8
       </SafeAreaView>
     )
   }
