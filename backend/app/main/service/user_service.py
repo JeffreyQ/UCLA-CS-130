e7e66054b26410e231b2995b8024a8ea5ceff7e1
Ram Goli <ramsgoli@gmail.com>
2019-11-22 16:30:11 -0800

    GET /users basic

--- backend/app/main/service/user_service.py
+++ backend/app/main/service/user_service.py
@@ -5,20 +5,22 @@ def save_new_user(data):
     try:
         email = data['email']
         user = User(email=email)
+
         db.session.add(user)
         db.session.commit()
 
-        return 200
+        response_object = {
+            'status': 'success',
+            'message': 'Successfully registered.'
+        }
+
+        return response_object, 201
     except Exception as e:
         print(e)
         return 500
 
 def get_all_users():
-    asdf = User.query.all()
-    for user in asdf:
-        print(user)
-    return "Working!"
-
+    return User.query.all()
 
 def get_a_user():
     pass
