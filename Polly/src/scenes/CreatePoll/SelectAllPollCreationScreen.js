fda995708ad7fe93bd8b8a1931a2bd1d5d953981
Jeffrey Qiu <jeffreyyqiu@gmail.com>
2019-11-30 17:05:19 -0800

    connect select all that apply poll creation page to backend

--- Polly/src/scenes/CreatePoll/SelectAllPollCreationScreen.js
+++ Polly/src/scenes/CreatePoll/SelectAllPollCreationScreen.js
@@ -4,10 +4,25 @@ import { ThemeProvider, Button } from 'react-native-elements'
 import { theme, styles } from './index'
 
 export default class SelectAllPollCreationScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      formType: 'selectAll',
+      prompt: '',
+      option1: '',
+      option2: '',
+      option3: '',
+      option4: ''
+    }
+  }
+  
   static navigationOptions = {
     title: 'Select All Poll',
   };
   render() {
+    JSONWebToken = this.props.navigation.getParam('JSONWebToken')
+    createPoll = this.props.navigation.getParam('createPoll')
+    const { option1, option2, option3, option4 } = this.state
     return(
       <ThemeProvider theme={theme}>
         <View style={{padding: 15, flex:1}}>
@@ -17,6 +32,7 @@ export default class SelectAllPollCreationScreen extends React.Component {
               multiline={true}
               scrollEnabled={true}
               style={styles.input}
+              onChangeText={prompt => this.setState({prompt})}
             />
           </View>
         </View>
@@ -26,6 +42,7 @@ export default class SelectAllPollCreationScreen extends React.Component {
             <TextInput
               multiline={true}
               style={styles.optionInput}
+              onChangeText={option1 => this.setState({option1})}
             />
           </View>
           <Text style={{fontSize:14, paddingTop: 6, paddingBottom:6}}>Option 2</Text>
@@ -33,6 +50,7 @@ export default class SelectAllPollCreationScreen extends React.Component {
             <TextInput
               multiline={true}
               style={styles.optionInput}
+              onChangeText={option2 => this.setState({option2})}
             />
           </View>
           <Text style={{fontSize:14, paddingTop: 6, paddingBottom:6}}>Option 3</Text>
@@ -40,6 +58,7 @@ export default class SelectAllPollCreationScreen extends React.Component {
             <TextInput
               multiline={true}
               style={styles.optionInput}
+              onChangeText={option3 => this.setState({option3})}
             />
           </View>
           <Text style={{fontSize:14, paddingTop: 6, paddingBottom:6}}>Option 4</Text>
@@ -47,6 +66,7 @@ export default class SelectAllPollCreationScreen extends React.Component {
             <TextInput
               multiline={true}
               style={styles.optionInput}
+              onChangeText={option4 => this.setState({option4})}
             />
           </View>
         </View>
@@ -54,7 +74,15 @@ export default class SelectAllPollCreationScreen extends React.Component {
           <Button
             style={styles.container}
             title="Create"
-            onPress={this.props.navigation.getParam('createPoll')}
+            onPress={() => createPoll({
+              ...this.state,
+              respStruct: {
+                option1: option1,
+                option2: option2,
+                option3: option3,
+                option4: option4
+              }
+            }, JSONWebToken)}
           />
         </View>
       </ThemeProvider>
