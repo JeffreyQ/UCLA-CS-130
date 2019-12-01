e01959601d0bbbdb5b52c38ad41c1fec7ceef491
Stephanie Yip <sjyip@ucla.edu>
2019-12-01 14:32:51 -0800

    Basic Elements of Subscriber/Subscription page working

--- Polly/src/scenes/Profile/subscribe.js
+++ Polly/src/scenes/Profile/subscribe.js
@@ -1,16 +1,56 @@
 import React from 'react'
-import { SafeAreaView, View, StyleSheet, Text, TouchableHighlight, ScrollView } from 'react-native'
+import { SafeAreaView, View, StyleSheet, Text, TouchableHighlight} from 'react-native'
+import { ScrollView } from 'react-navigation'
 
 import { heading2Text, bodyText, grayBody } from '../../textMixins'
+import SubscribeEntry from './subscribeEntry'
 
+const users = [
+  {
+    id:1,
+    name: 'Stephanie Y',
+    image: require('./profile.jpg')
+  },
+  {
+    id:2,
+    name: 'Ram G.',
+    image: require('./profile.jpg')
+  },
+  {
+    id:3,
+    name: 'Jeff Q.',
+    image: require('./profile.jpg')
+  },
+  {
+    id:4,
+    name: 'Jesse C.',
+    image: require('./profile.jpg')
+  },
+  {
+    id:5,
+    name: 'Lawrence C.',
+    image: require('./profile.jpg')
+  },
+]
+const numUsers = users.length
 
 class SubscriptionScreen extends React.Component{
 
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <View style ={styles.container}>
-            <Text style={{...heading2Text}}> 89 Pollers</Text>
+        <View style ={styles.headingContainer}>
+            <Text style={styles.heading}> {numUsers} Pollers </Text>
+        </View>
+        <View style={styles.scrollContainer}>
+          <ScrollView
+            style={{
+              height:'100%',
+              alignSelf:'stretch',
+              padding: 20,
+            }}>
+              {users.map(user => <SubscribeEntry key={user.id} user={user}/>)}
+          </ScrollView>
         </View>
       </SafeAreaView>
     )
@@ -22,8 +62,18 @@ class SubscriberScreen extends React.Component{
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <View style ={styles.container}>
-            <Text style={{...heading2Text}}> 89 Pollees</Text>
+        <View style ={styles.headingContainer}>
+            <Text style={styles.heading}> {numUsers} Pollees </Text>
+        </View>
+        <View style={styles.scrollContainer}>
+          <ScrollView
+            style={{
+              height:'100%',
+              alignSelf:'stretch',
+              padding: 20,
+            }}>
+              {users.map(user => <SubscribeEntry key={user.id} user={user}/>)}
+          </ScrollView>
         </View>
       </SafeAreaView>
     )
@@ -33,8 +83,16 @@ export default SubscriptionScreen
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     padding: 20,
+  },
+  headingContainer: {
+    padding: 20,
+    alignItems: 'center',
+    borderBottomWidth: 0.4,
+    borderBottomColor: '#A5A3A3'
+  },
+  scrollContainer: {
+    padding: 10,
     borderBottomWidth: 0.4,
     borderBottomColor: '#A5A3A3'
   },
@@ -47,7 +105,7 @@ const styles = StyleSheet.create({
     borderRadius: 50,
   },
   heading: {
-    justifyContent: 'space-between',
+    ...heading2Text,
     flexDirection: 'row',
     alignItems: 'center'
   },
