d65501d9ffbd1fa07dc03e019309bd0268095481
Nityan <nityanbhorde21@g.ucla.edu>
2019-12-03 03:22:48 -0800

    issue 33

--- backend/app/main/controller/poll_controller.py
+++ backend/app/main/controller/poll_controller.py
@@ -58,7 +58,7 @@ class PollsFollowing(Resource):
         print(user)
         return poll_service.get_polls_following(user.id)
 
-@api.route('/response/<poll_id>')
+@api.route('/<poll_id>/response')
 class PollResponse(Resource):
     @jwt_required
     @api.marshal_with(_poll.get_all_responses_poll)
@@ -76,4 +76,4 @@ class PollResponse(Resource):
         '''post a response to given poll id'''
         data = request.json
         user = get_current_user()
-        return poll_service.respond_to_poll(2,poll_id,data)
+        return poll_service.respond_to_poll(user.id,poll_id,data)
